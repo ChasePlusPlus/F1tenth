8715645539fb74f3ec9928f15a29ab4b9d1d4c43
Chase Deets <chd5hq@virginia.edu>
2018-02-01 00:23:03 -0500

    Create problem4.py

---
+++ Assignment1/problem4.py
@@ -0,0 +1,15 @@
+import copy
+
+#create an input list, swap commented lists to test both cases
+myList = [0,1,2,3,4,5] #sorted, should return true
+#myList = [1,5,4,3,0,2] #unsorted, should return false
+
+#create a deep copy of input list and sort
+sortList = copy.copy(myList)
+sortList.sort()
+
+#if the input and sorted lists match, return true, else ret false
+if myList == sortList:
+  print "Yes, the list is sorted"
+else:
+  print "No, the list is not in order"
