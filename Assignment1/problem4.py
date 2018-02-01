e0a6e99036069a10553bbc3638f212419e26c30f
Chase Deets <chd5hq@virginia.edu>
2018-02-01 01:20:20 -0500

    updated with more test cases

--- Assignment1/problem4.py
+++ Assignment1/problem4.py
@@ -1,8 +1,10 @@
 import copy
 
-#create an input list, swap commented lists to test both cases
+#create an input list, swap between uncommented lists to test all cases
 myList = [0,1,2,3,4,5] #sorted, should return true
 #myList = [1,5,4,3,0,2] #unsorted, should return false
+#myList = [0,0,1,1,2,2,3,3] #sorted with repeated adjacent values, should return true
+#myList = [0,0,1,1,3,3,2,2] #unsorted with repeated adjacent values, should return false
 
 #create a deep copy of input list and sort
 sortList = copy.copy(myList)
